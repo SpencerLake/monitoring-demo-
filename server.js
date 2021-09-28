22e613295ded499eab002af11fff663bb5cd1791
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-09-28 14:05:25 -0400

    updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {TooManyRequests} = require('http-errors')
+const { TooManyRequests } = require('http-errors')
 const path = require('path')
 const app = express()
 
