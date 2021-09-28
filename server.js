d43a8d44703cb40b186c92e9f92b45afab0b61ce
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-09-28 14:26:41 -0400

    added student endpoint and front end

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.get('/', (req,res) => {
     rollbar.info("Html was monitored successfully")
 })
 
+const studentArr = []
+
+app.post('/api/students', (req,res) => {
+    const {name} = req.body
+    // const name = re.body.name These are the same but the other one is destructering
+    studentArr.push(name)
+
+    rollbar.log('Student successfully added')
+
+    res.status(200).send(studentArr)
+})
+
 const port = process.env.PORT || 5656
 
 app.use(rollbar.errorHandler())
